e0396183705bce2b034a0ce7eef0f560141eba30
Rizwan Mahmood <rizwanmahmood255@gmail.com>
2019-01-22 12:05:56 +0000

    Finished Anagrams

--- Level3/src/Anagrams.java
+++ Level3/src/Anagrams.java
@@ -2,7 +2,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.AbstractCollection;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,7 +18,7 @@ public class Anagrams {
 		List<String> words;
 		try {
 			words = Files.readAllLines(filePath);
-			return words;
+			return words.stream().map(String::toLowerCase).collect(Collectors.toList());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -32,19 +36,58 @@ public class Anagrams {
 		return output;
 	}
 	
-	private static List<String> sortWordsAlphabetically(List<String> unsortedWords) {
+	private static List<String> sortWordsAlphabetically(Collection<String> unsortedWords) {
 		return unsortedWords.stream()
-				.map(Anagrams::sortString)
-				.collect(Collectors.toList());
+			.map(Anagrams::sortString)
+			.collect(Collectors.toList());
 	}
 	
-	public static String findWordWithMostAnagramsFrom(String fileName) {
+	
+	private static int getMaxNumberOfAnagrams(Collection<ArrayList<String>> anagrams) {
+		return anagrams.stream()
+			.max(Comparator.comparing(ArrayList::size))
+			.map(ArrayList::size)
+			.get().intValue();
+	}
+	
+	private static List<ArrayList<String>> collectCommonAnagramsOfSize(Collection<ArrayList<String>> anagrams, int size) {
+		return anagrams.stream()
+			.filter((list) -> list.size() == size)
+			.collect(Collectors.toList());
+	}
+	
+	private static int getLengthOfLongestAnagram(Collection<ArrayList<String>> anagrams) {
+		return anagrams.stream()
+			.mapToInt(list -> list.get(0).length())
+			.max().getAsInt();
+	}
+	
+	private static List<ArrayList<String>> collectAnagramsOfSize(Collection<ArrayList<String>> anagrams, int size) {
+		return anagrams.stream()
+			.filter(list -> list.get(0).length() == size)
+			.collect(Collectors.toList());
+	}
+	
+	public static List<ArrayList<String>> findWordWithMostAnagramsFrom(String fileName) {
 		List<String> wordsFromFile = readWordsFromFile(fileName);
 		List<String> sortedWords = sortWordsAlphabetically(wordsFromFile);
 		
-		HashMap<String, Integer> wordCount = new HashMap<>(); 
+		HashMap<String, ArrayList<String>> sortedWordToAnagrams = new HashMap<>();
+		sortedWords.forEach(word -> sortedWordToAnagrams.put(word, new ArrayList<>()));
+		wordsFromFile.forEach(word -> sortedWordToAnagrams.get(sortString(word)).add(word));
 		
-
-		return "";
+		Collection<ArrayList<String>> anagrams = sortedWordToAnagrams.values();
+		
+		int maxNumberOfAnagrams = getMaxNumberOfAnagrams(anagrams);
+		
+		List<ArrayList<String>> commonAnagrams = collectCommonAnagramsOfSize(anagrams, maxNumberOfAnagrams);
+		
+		int longestLengthAnagram = getLengthOfLongestAnagram(commonAnagrams);
+		
+		List<ArrayList<String>> bestAnagrams = collectAnagramsOfSize(commonAnagrams, longestLengthAnagram);
+		
+		System.out.println(bestAnagrams);
+		
+		return bestAnagrams;
 	}
 }
